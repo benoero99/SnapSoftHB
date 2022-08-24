86b3160e1a8b17ea6f8ebd7f243b2318d29dd39c
benoero99 <61589994+benoero99@users.noreply.github.com>
2022-08-24 14:11:14 +0200

    Adding searchbar in movies fragment. Added KeyboardUtils for keyboard hide.

--- app/src/main/java/com/example/snapsofthb/network/TMDBApi.kt
+++ app/src/main/java/com/example/snapsofthb/network/TMDBApi.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface TMDBApi {
 
-    @GET("https://api.themoviedb.org/3/movie/popular?api_key=86d5c00ba4de5bbc45f600df22dc125c")
+    @GET("movie/popular?api_key=86d5c00ba4de5bbc45f600df22dc125c")
     suspend fun getPopularMovies() : MoviesResult
 }
--- app/src/main/java/com/example/snapsofthb/ui/MovieFragment.kt
+++ app/src/main/java/com/example/snapsofthb/ui/MovieFragment.kt
@@ -1,28 +1,60 @@
 package com.example.snapsofthb.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.extensions.exhaustive
 import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
-import com.example.snapsofthb.R
+import com.example.snapsofthb.databinding.FragmentMovieBinding
+import com.example.snapsofthb.util.KeyboardUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MovieFragment : RainbowCakeFragment<MovieViewState, MovieViewModel>() {
+
+    private var _binding: FragmentMovieBinding? = null
+    private val binding get() = _binding!!
+
     override fun provideViewModel() = getViewModelFromFactory()
-    override fun getViewResource() = R.layout.fragment_movie
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentMovieBinding.inflate(inflater,container, false)
+        return binding.root
+    }
 
-        viewModel.load()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.certificateListSearcherTIET.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                binding.certificateListSearcherTIET.clearFocus()
+                KeyboardUtils.hideKeyboard(activity)
+                //viewModel.load()
+                true
+            } else false
+        }
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun render(viewState: MovieViewState) {
         when(viewState) {
             Loading -> Toast.makeText(requireContext(), "View is loading", Toast.LENGTH_SHORT).show()
-            is Ready -> Toast.makeText(requireContext(), viewState.moviesResult.toString(), Toast.LENGTH_SHORT).show()
+            is Ready -> {
+                Toast.makeText(requireContext(), viewState.moviesResult.toString(), Toast.LENGTH_SHORT).show()
+
+            }
         }.exhaustive
     }
 
